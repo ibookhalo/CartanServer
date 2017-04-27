e685d500661743b62492d973ea03efb9ac5d1b77
IbooKhalo <ibo.khalo@hotmail.com>
2017-04-27 13:33:34 +0200

    test

--- CartanServer/Program.cs
+++ CartanServer/Program.cs
@@ -9,7 +9,7 @@ namespace CartanServer
         {
             CartanTcpServer s = new CartanTcpServer(new IPEndPoint( IPAddress.Parse("127.0.0.1"),123));
             s.Start();
-            
+            //
         }
     }
 }
